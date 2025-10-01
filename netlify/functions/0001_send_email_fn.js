4ab993b2bbfb513982cfbcaaa361298c868b6f58
Mohit Paudyal <ak47@Mohits-MacBook-Pro.attlocal.net>
2025-10-01 18:40:57 -0500

    add support for lint

--- netlify/functions/0001_send_email_fn.js
+++ netlify/functions/0001_send_email_fn.js
@@ -11,7 +11,7 @@ const mailerSend = new MailerSend({
 });
 
 const sentFrom = new Sender(process.env.MAILERSEND_FROM_EMAIL);
-
+dsdf;
 export const handler = async (event) => {
   if (event.httpMethod !== "POST") {
     return {

16dfd17dd1a794ae7c2808ae7ba7ca1cfa8af8e7
mohit2530 <mohit.paudyal@gmail.com>
2025-12-22 21:15:11 -0600

    Added the ability to add apiFields into the template
    - Added util function to build apiFields into the provided template
    - Added support for signer fields and also added some cleanup.
    
    Added support to view checkboxes in correct format
    
    - after the initial review, we found out the checkboxes on the goodsign are expected to contain the tick, we
    were passing true or false and it was just printing true or false out. rather it should be tick or nothing.
    
    - also we needed to clean up the esign template since each field is unique. hence we have to pass fields as
    how the page is expected to be drafted. I thought we could pass dynamic values but that does not seem to be the case.
    Added support for this revamp.

--- netlify/functions/utils/utils.js
+++ netlify/functions/utils/utils.js
@@ -7,12 +7,16 @@ const GoodSignBaseUrl = "https://goodsign.io/api";
 
 const EsignWorkspaceUri = "/workspaces";
 const EsignTemplatesUri = "/templates";
+const GoodSignTemplateToEsignUri = "/usetemplate";
+
+const DefaultReminderDays = 3;
 
 export const EsignWorkspaceUrl = EsignBaseUrl + EsignWorkspaceUri;
 export const EsignTemplatesUrl = EsignBaseUrl + EsignTemplatesUri;
 
 export const GoodSignTemplatesUrl = GoodSignBaseUrl + EsignTemplatesUri;
-
+export const GoodSignTemplateToEsignUrl =
+  GoodSignBaseUrl + GoodSignTemplateToEsignUri;
 /**
  * initializeFirebase ...
  *
@@ -62,4 +66,182 @@ export const populateCorsHeaders = () => {
     "Access-Control-Allow-Methods": "POST, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type",
   };
+};
+
+/**
+ * populateApiFields ...
+ *
+ * used to populate api fields for residential lease for goodsign api
+ *
+ * @returns Object - default api fields
+ */
+export const populateApiFields = (fields) => {
+  const fetchValue = (key, valueType, isTenantSigner = false) => {
+    if (fields[key] === undefined) return null;
+    switch (valueType) {
+      case "string":
+        return {
+          key,
+          value: String(fields[key]),
+          info_current_value: "text",
+          info_current_contact: "",
+          info_current_type: "label",
+          info_current_subtype: "",
+        };
+      case "boolean":
+        return {
+          key,
+          value: Boolean(fields[key]) ? "\u2713" : "",
+          info_current_value: Boolean(fields[key]) ? "\u2713" : "",
+          info_current_contact: "propertyowner@temp.template",
+          info_current_type: "input",
+          info_current_subtype: "checkbox",
+        };
+
+      case "dateTime":
+        return {
+          key,
+          value: String(fields[key]),
+          info_current_value: "dd\/mm\/yyyy",
+          info_current_contact: isTenantSigner
+            ? "tenant@temp.template"
+            : "propertyowner@temp.template",
+          info_current_type: "input",
+          info_current_subtype: "datesigned",
+        };
+      default:
+        return "N/A";
+    }
+  };
+
+  const draft = Object.fromEntries(
+    Object.entries({
+      county: fetchValue("county", "string"),
+      startDate: fetchValue("startDate", "string"),
+      endDate: fetchValue("endDate", "string"),
+      address: fetchValue("address", "string"),
+      isAutoRenew: fetchValue("isAutoRenew", "boolean"),
+      autoRenewDays: fetchValue("autoRenewDays", "string"),
+      isMonthLastDate: fetchValue("isMonthLastDate", "string"),
+      rent: fetchValue("rent", "string"),
+      isFirstDayRent: fetchValue("isFirstDayRent", "boolean"),
+      rentDueDate: fetchValue("rentDueDate", "string"), // like 4 days after the lease is signed
+      isPayToLandlord: fetchValue("isPayToLandlord", "boolean"),
+      isPayToListingBroker: fetchValue("isPayToListingBroker", "boolean"),
+      isPayToPropertyManager: fetchValue("isPayToPropertyManager", "boolean"),
+      isCashiersCheck: fetchValue("isCashiersCheck", "boolean"),
+      isElectronicPayment: fetchValue("isElectronicPayment", "boolean"),
+      isMoneyOrder: fetchValue("isMoneyOrder", "boolean"),
+      isPersonalCheck: fetchValue("isPersonalCheck", "boolean"),
+      isOtherMeans: fetchValue("isOtherMeans", "boolean"),
+      owner: fetchValue("owner", "string"),
+      tenant: fetchValue("tenant", "string"),
+      address: fetchValue("address", "string"),
+      proratedRent: fetchValue("proratedRent", "string"),
+      proratedRentDueDate: fetchValue("proratedRentDueDate", "string"),
+      isCashiersCheck: fetchValue("isCashiersCheck", "boolean"),
+      isElectronicPayment: fetchValue("isElectronicPayment", "boolean"),
+      isMoneyOrder: fetchValue("isMoneyOrder", "boolean"),
+      isPersonalCheck: fetchValue("isPersonalCheck", "boolean"),
+      isOtherMeans: fetchValue("isOtherMeans", "boolean"),
+      owner: fetchValue("owner", "string"),
+      paymentID: fetchValue("paymentID", "string"),
+      isCashiersCheck: fetchValue("isCashiersCheck", "boolean"),
+      isElectronicPayment: fetchValue("isElectronicPayment", "boolean"),
+      isMoneyOrder: fetchValue("isMoneyOrder", "boolean"),
+      isPersonalCheck: fetchValue("isPersonalCheck", "boolean"),
+      isOtherMeans: fetchValue("isOtherMeans", "boolean"),
+      isCashiersCheck: fetchValue("isCashiersCheck", "boolean"),
+      isElectronicPayment: fetchValue("isElectronicPayment", "boolean"),
+      isMoneyOrder: fetchValue("isMoneyOrder", "boolean"),
+      isPersonalCheck: fetchValue("isPersonalCheck", "boolean"),
+      isOtherMeans: fetchValue("isOtherMeans", "boolean"),
+      isExtraChargeNotAdded: fetchValue("isExtraChargeNotAdded", "boolean"),
+      isMonthlyPaymentsRequired: fetchValue(
+        "isMonthlyPaymentsRequired",
+        "boolean",
+      ),
+      address: fetchValue("address", "string"),
+      rentPaymentDeadline: fetchValue("rentPaymentDeadline", "string"),
+      initialLateFee: fetchValue("initialLateFee", "string"),
+      dailyLateFee: fetchValue("dailyLateFee", "string"),
+      isInitialLateFee: fetchValue("isInitialLateFee", "boolean"),
+      returnedPaymentFee: fetchValue("returnedPaymentFee", "string"),
+      initialAnimalViolationFee: fetchValue(
+        "initialAnimalViolationFee",
+        "string",
+      ),
+      dailyAnimalViolationFee: fetchValue("dailyAnimalViolationFee", "string"),
+      address: fetchValue("address", "string"),
+      securityDeposit: fetchValue("securityDeposit", "string"),
+      isCashiersCheck: fetchValue("isCashiersCheck", "boolean"),
+      isElectronicPayment: fetchValue("isElectronicPayment", "boolean"),
+      isMoneyOrder: fetchValue("isMoneyOrder", "boolean"),
+      isPersonalCheck: fetchValue("isPersonalCheck", "boolean"),
+      isOtherMeans: fetchValue("isOtherMeans", "boolean"),
+      address: fetchValue("address", "string"),
+      ownerCoveredUtilities: fetchValue("ownerCoveredUtilities", "string"),
+      address: fetchValue("address", "string"),
+      isHoa: fetchValue("isHoa", "boolean"),
+      isNotHOA: fetchValue("isNotHOA", "boolean"),
+      hoaDetails: fetchValue("hoaDetails", "boolean"),
+      guestsPermittedStayDays: fetchValue("guestsPermittedStayDays", "string"),
+      allowedVehicleCounts: fetchValue("allowedVehicleCounts", "string"),
+      address: fetchValue("address", "string"),
+      tripCharge: fetchValue("tripCharge", "string"),
+      allowKeyboxSince: fetchValue("allowKeyboxSince", "string"),
+      removeKeyboxFee: fetchValue("removeKeyboxFee", "string"),
+      address: fetchValue("address", "string"),
+      inventoryCompleteWithin: fetchValue("inventoryCompleteWithin", "string"),
+      address: fetchValue("address", "string"),
+      isTenantCleaningYard: fetchValue("isTenantCleaningYard", "boolean"),
+      address: fetchValue("address", "string"),
+      isSmokingNotAllowed: fetchValue("isSmokingNotAllowed", "boolean"),
+      emergencyContactNumber: fetchValue("emergencyContactNumber", "string"),
+      address: fetchValue("address", "string"),
+      address: fetchValue("address", "string"),
+      address: fetchValue("address", "string"),
+      address: fetchValue("address", "string"),
+      specialProvisions: fetchValue("specialProvisions", "string"),
+      address: fetchValue("address", "string"),
+      rentalFloodDisclosure: fetchValue("rentalFloodDisclosure", "boolean"),
+      tenant: fetchValue("tenant", "string"),
+      owner: fetchValue("owner", "string"),
+      tenantEmail: fetchValue("tenantEmail", "string"),
+      ownerEmail: fetchValue("ownerEmail", "string"),
+      address: fetchValue("address", "string"),
+      city: fetchValue("city", "string"),
+      state: fetchValue("state", "string"),
+      zipCode: fetchValue("zipCode", "string"),
+      brokerName: fetchValue("brokerName", "string"),
+      isBrokerManaged: fetchValue("isBrokerManaged", "boolean"),
+      isNotBrokerManaged: fetchValue("isNotBrokerManaged", "boolean"),
+      isOwnerManaged: fetchValue("isOwnerManaged", "boolean"),
+      isManagerManaged: fetchValue("isManagerManaged", "boolean"),
+      managerName: fetchValue("managerName", "string"),
+      managerAddress: fetchValue("managerAddress", "string"),
+      managerPhone: fetchValue("managerPhone", "string"),
+      datesigned1: fetchValue("dateSigned1", "dateTime"),
+      datesigned2: fetchValue("dateSigned2", "dateTime", true),
+    }).filter(([_, v]) => v !== null),
+  );
+
+  return draft;
+};
+
+/**
+ * populateSignerFields...
+ *
+ * used to populate the signer fields for the signing parties
+ */
+export const populateSignerFields = (key, name, email) => {
+  return Object.assign(
+    {},
+    {
+      key,
+      name,
+      email,
+      reminder_days: DefaultReminderDays,
+    },
+  );
 };
